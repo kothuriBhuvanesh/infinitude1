14cd2b439224a61936401ea35826640b83d3ea2c
kothuriBhuvanesh <164981463+kothuriBhuvanesh@users.noreply.github.com>
2024-05-23 12:21:32 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,17 @@
+import calculator
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+
+    return x * y
+
+def divide(x, y):
+    return x / y
+print("sum:",calculator.add(2, 5))
+print("sub:",calculator.subtract(5,8))
+print("mul:",calculator.multiply(8,2))
+
